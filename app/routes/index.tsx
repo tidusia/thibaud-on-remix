1818a0a61499a0c9bed9147f37dae5cbd5109b49
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2021-11-04 21:07:02 +0100

    feat: introduce state

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
 import { useLoaderData } from "remix";
 import { Link } from "react-router-dom";
@@ -21,6 +22,7 @@ export let loader: LoaderFunction = async () => {
 
 export default function Index() {
   let data = useLoaderData();
+  const [count, setCount] = React.useState(0)
 
   return (
     <div style={{ textAlign: "center", padding: 20 }}>
@@ -29,6 +31,7 @@ export default function Index() {
         <a href="https://docs.remix.run">Check out the docs</a> to get started.
       </p>
       <p>Message from the loader: {data.message}</p>
+      <button onClick={() => setCount(prevState => prevState + 1)}>Count : {count}</button>
       <p>
         <Link to="not-found">Link to 404 not found page.</Link> Clicking this
         link will land you in your root CatchBoundary component.
